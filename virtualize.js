0699b55648beb51f92502803edc4e9553b5b1d2f
Pranit Nigde <52257513+pranitnigde@users.noreply.github.com>
2020-03-14 01:19:03 +0530

    final project with validation

--- virtualize.js
+++ virtualize.js
@@ -641,7 +641,7 @@ function init(){
 	switch(algorithm){
 		case 'Dijkstra':
 		$("#Virtualize_button").css({ 'background-color' : '#c20000'});
-			// clearpath();
+		        clearpath();
 			var x = document.getElementById("snackbar")
 			x.className = "show";
 			setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
